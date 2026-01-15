8e63e4fc9de612f3a63080ea6556fc3da2f709ca
Cameron Berryman <91347395+CB12354@users.noreply.github.com>
2026-01-15 20:07:10 +0000

    Lab commit 3

--- kqe6rfday2.py
+++ kqe6rfday2.py
@@ -5,6 +5,14 @@
 # %%
 # Setup
 # Path: @CB12354 -> /workspaces/daytwostuff (main)
+
+
+# %% Virtual environment setup
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip3 install -r requirements.txt
+
+# Don't include .venv in the repo, just commit the requirements.txt so that the user can install it on their own
 # Path after venv: (.venv) @CB12354 -> /workspaces/daytwostuff (main)
 
 # %%
@@ -31,11 +39,13 @@ penguins.head()
 
 # %% Codespaces for Dummies: Making a New Environment
 # 
-# Step 1: Create or fork a GitHub repo
+# Step 1: Create or fork a GitHub repo.
 #   - If a different base than normal linux for the dev container is required, add the dev container json and dockerfile
+#   - Make sure to set the .gitignore template to Python
 # Step 2: On the main Code page of the repo, click the green Code button, then Codespaces, then make a new codespace
 # Step 3: Go to VSCode Desktop, do >Codespaces: Connect to Codespace, log into your GitHub account and select that codespace you just made online
 # Step 4: Install necessary extensions to run the project (ex. Python, Data wrangler, etc.)
 # Step 5: Create and activate a virtual environment for Python projects
 # Step 6: Install needed dependencies and freeze them into a requirements file
-# Step 7: Add all modified files, commit, and push to the remote repo to save all locally made changes
+# Step 7: Add necessary ignored files or folders, like .venv, to a .gitignore file
+# Step 8: Add all modified files, commit, and push to the remote repo to save all locally made changes
