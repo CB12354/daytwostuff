1493320d2709e19a80d8809c2d0cec42c339ce04
Cameron Berryman <91347395+CB12354@users.noreply.github.com>
2026-01-15 19:44:11 +0000

    Lab commit 2

--- kqe6rfday2.py
+++ kqe6rfday2.py
@@ -28,3 +28,14 @@ penguins.head()
 # Requirements files ensure that other users get the exact necessary packages needed to run the file.
 # They may be missing a package or running the incorrect version of a package if they install everything on their own.
 # pip freeze > requirements.txt
+
+# %% Codespaces for Dummies: Making a New Environment
+# 
+# Step 1: Create or fork a GitHub repo
+#   - If a different base than normal linux for the dev container is required, add the dev container json and dockerfile
+# Step 2: On the main Code page of the repo, click the green Code button, then Codespaces, then make a new codespace
+# Step 3: Go to VSCode Desktop, do >Codespaces: Connect to Codespace, log into your GitHub account and select that codespace you just made online
+# Step 4: Install necessary extensions to run the project (ex. Python, Data wrangler, etc.)
+# Step 5: Create and activate a virtual environment for Python projects
+# Step 6: Install needed dependencies and freeze them into a requirements file
+# Step 7: Add all modified files, commit, and push to the remote repo to save all locally made changes
